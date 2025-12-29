98248e68b783b15e586a4a07174a10b708bb2cc9
abhishek <abhishekf@researchnxt.com>
2025-12-29 12:02:23 +0530

    Limit accounts grid to three columns

--- components/tabs/accounts-tab.tsx
+++ components/tabs/accounts-tab.tsx
@@ -262,7 +262,7 @@ export function AccountsTab({
                   </TableBody>
                 </Table>
               ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
                   {getPaginatedData(sortedAccounts, currentPage, itemsPerPage).map(
                     (account, index) => (
                       <AccountGridCard
